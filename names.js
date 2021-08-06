2ed769df3aa2af4f6e30eb46b29d3bb1b334fb67
sw88tch <fcukmycopyrights@gmail.com>
2021-08-06 14:32:27 +0300

    js first steps

---
+++ names.js
@@ -0,0 +1,8 @@
+var names = ['John', 'Kirill', 'Tom', 'Anton'];
+names.splice(2, 0, 'Jim');
+console.log(names)
+names.splice(-1, 1);
+console.log(names)
+var arr = [1, 2, 3];
+arr.shift()
+console.log(arr)
