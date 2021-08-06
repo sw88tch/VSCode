7ed8a8fa78cb5cff741b05c08d5aecf14f5724a7
sw88tch <fcukmycopyrights@gmail.com>
2021-08-06 14:34:10 +0300

    js first steps

---
+++ test_lesson.js
@@ -0,0 +1,14 @@
+var arr = [1, 5, 9, 7, 15];
+var max = arr[0]
+const b = 10
+for (let i = 0; i < arr.length; i++) {
+  var a = 5
+ //  const b = 10
+  if (max < arr[i]) {
+    max = arr[i]
+  }
+}
+console.log(max)
+console.log(a)
+console.log(b)
+console.log(i)
