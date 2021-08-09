27ffe1573acbd42234d278b6adf0ea4b870f39ab
sw88tch <fcukmycopyrights@gmail.com>
2021-08-09 19:52:40 +0300

    cypress first steps

---
+++ Cypress-Automation-Framework/cypress/integration/automation-test-store/contact_us.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe("Test Contacs Us form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://automationteststore.com/")
+    })
+})
