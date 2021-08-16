290cec4b2fc42d90ee5532ae2745d60524c71c3a
sw88tch <fcukmycopyrights@gmail.com>
2021-08-16 13:03:56 +0300

    update file

--- Cypress-Automation-Framework/cypress/integration/webdriver-uni/contact_us.js
+++ Cypress-Automation-Framework/cypress/integration/webdriver-uni/contact_us.js
@@ -1,31 +1,33 @@
-/// <reference types="Cypress" />
+import Homepage_PO from '../../support/pageObjects/webdriver-uni/Homepage_PO'
+import Contact_Us_PO from '../../support/pageObjects/webdriver-uni/Contact_Us_PO'
 
-describe("Test Contacs Us form via Webdriver Uni", () => {
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+    Cypress.config('defaultCommandTimeout', 20000)
+    const homepage_PO = new Homepage_PO();
+    const contact_Us_PO = new Contact_Us_PO();
+
     before(function () {
-        cy.fixture('example').then(function (data) {
-            // this.data = data
-            globalThis.data = data
-        })
-    })
-    beforeEach(function () {
-        cy.visit(Cypress.env("webdriveruni_homepage") + "/Contact-Us/contactus.html")
-        
-    })
-    it("Should be able to submit a successful submission via contact us form", () => {
-        cy.get('[name="first_name"]').type("Joe")
-        cy.get('[name="last_name"]').type("Bowey")
-        cy.get('[name="email"]').type("joebowey@ya.hoo")
-        cy.get('textarea.feedback-input').type("Comment for this field")
-        cy.get('[type="submit"]').click()
-    })
+        cy.fixture("example").then(function (data) {
+            //this.data = data;
+            globalThis.data = data;
+        });
+    });
 
-    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cypress code
-        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-        cy.get('[name="first_name"]').type("Joe")
-        cy.get('[name="last_name"]').type("Bowey")
-        cy.get('textarea.feedback-input').type("Comment for this field")
-        cy.get('[type="submit"]').click()
-        cy.get('body').contains('Error: all fields are required')
-    })
-})
+    beforeEach(function () {
+        homepage_PO.visitHompage();
+        homepage_PO.clickOn_ContactUs_Button();
+    });
+
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.document().should("have.property", "charset").and("eq", "UTF-8");
+        cy.title().should("include", "WebDriver | Contact Us");
+        cy.url().should("include", "contactus");
+        contact_Us_PO.contactForm_Submission(Cypress.env("first_name"), data.last_name, data.email, "How can I learn Cypress?", "h1", "Thank You for your Message!");
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        contact_Us_PO.contactForm_Submission(data.first_name, data.last_name, " ", "How can I learn Cypress?", "body", "Error: Invalid email address")
+    });
+});
