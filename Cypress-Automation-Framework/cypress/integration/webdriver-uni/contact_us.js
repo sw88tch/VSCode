c2a7451c76e520ca93ac8f699f09ab1a64192452
sw88tch <fcukmycopyrights@gmail.com>
2021-08-16 11:48:52 +0300

    update file

--- Cypress-Automation-Framework/cypress/integration/webdriver-uni/contact_us.js
+++ Cypress-Automation-Framework/cypress/integration/webdriver-uni/contact_us.js
@@ -1,10 +1,17 @@
 /// <reference types="Cypress" />
 
 describe("Test Contacs Us form via Webdriver Uni", () => {
+    before(function () {
+        cy.fixture('example').then(function (data) {
+            // this.data = data
+            globalThis.data = data
+        })
+    })
+    beforeEach(function () {
+        cy.visit(Cypress.env("webdriveruni_homepage") + "/Contact-Us/contactus.html")
+        
+    })
     it("Should be able to submit a successful submission via contact us form", () => {
-        // cypress code
-        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-        // cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe")
         cy.get('[name="last_name"]').type("Bowey")
         cy.get('[name="email"]').type("joebowey@ya.hoo")
@@ -12,12 +19,13 @@ describe("Test Contacs Us form via Webdriver Uni", () => {
         cy.get('[type="submit"]').click()
     })
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+    it.only("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
         // cypress code
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
         cy.get('[name="first_name"]').type("Joe")
         cy.get('[name="last_name"]').type("Bowey")
         cy.get('textarea.feedback-input').type("Comment for this field")
         cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
     })
 })
