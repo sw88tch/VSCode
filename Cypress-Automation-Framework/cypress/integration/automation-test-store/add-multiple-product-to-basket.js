7f2a49821b8a035901bb21dd0df0475fb4d4d520
sw88tch <fcukmycopyrights@gmail.com>
2021-08-16 12:57:26 +0300

    update files with use PO

--- Cypress-Automation-Framework/cypress/integration/automation-test-store/add-multiple-product-to-basket.js
+++ Cypress-Automation-Framework/cypress/integration/automation-test-store/add-multiple-product-to-basket.js
@@ -1,8 +1,10 @@
 import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO'
+import AutoStore_HairCare_PO from '../../support/pageObjects/automation-test-store/AutoStore_HairCare_PO'
 /// <reference types="cypress" />
 
 describe("Add multiple items to basket", () => {
     const autoStore_Homepage_PO = new AutoStore_Homepage_PO()
+    const autoStore_HairCare_PO = new AutoStore_HairCare_PO()
     before(function () {
         cy.fixture("products").then(function (data) {
             globalThis.data = data;
@@ -14,9 +16,6 @@ describe("Add multiple items to basket", () => {
         autoStore_Homepage_PO.clickOn_HairCare_Link()
     });
     it("Add specific items to basket", () => {
-        globalThis.data.productName.forEach(function (element) {
-            cy.addProductToBasket(element)
-        })
-        cy.get('.dropdown-toggle > .fa').click();
+       autoStore_HairCare_PO.addHairCareProductsToBasket()
     });
 });
