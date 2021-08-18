6111a01634f0ce04c241f418cb9093b37e05479d
sw88tch <fcukmycopyrights@gmail.com>
2021-08-18 15:35:11 +0300

    add network request

---
+++ Cypress-Automation-Framework/cypress/integration/requests/network-request.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe("Network Requests", () => {
+    beforeEach(() => {
+        cy.visit("https://example.cypress.io/commands/network-requests")
+
+        // Start a server to begin routing responses to cy.route() and cy.request().
+        cy.server();
+    })
+    it("Get Request", () => {
+        // Listen to GET requests which use the following: comments/ within the url
+        cy.route("GET", "comments/*").as("getComment");
+
+        cy.get(".network-btn").click();
+        cy.wait("@getComment").its("status").should("eq", 200)
+    })
+})
