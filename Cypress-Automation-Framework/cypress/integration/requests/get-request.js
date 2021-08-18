dcff7f67b3b47ed014b5029edf047b7344e0a13d
sw88tch <fcukmycopyrights@gmail.com>
2021-08-18 14:07:36 +0300

    add get request

--- Cypress-Automation-Framework/cypress/integration/requests/get-request.js
+++ Cypress-Automation-Framework/cypress/integration/requests/get-request.js
@@ -6,4 +6,25 @@ describe("Get Request", () => {
         result = cy.request("http://localhost:3000/posts");
         result.its("status").should("equal", 200)
     })
+    it("Validate /posts api contains the correct keys and values", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:3000/posts",
+            headers: {
+                accept: "application/json"
+            }
+        }).then(response => {
+            let body = JSON.parse(JSON.stringify(response.body))
+            cy.log(body)
+
+            expect(body[0]).has.property("title", "Test")
+            expect(body[1]).has.property("author", "Teplov DD")
+
+            body.forEach(function(item) {
+                expect(item).to.have.all.keys("id", "title", "author");
+                cy.log("Author: " +item["author"] + " & Title: " + item["title"])
+            })
+        })
+        
+    })
 })
